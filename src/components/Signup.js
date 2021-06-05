53960ebac69b30167654d38986aabcf238982560
ogzhn <oguzhancavdar06@gmail.com>
2021-06-05 19:25:00 +0300

    add signup and textfield comp

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -1,10 +1,40 @@
-
+import { Formik , Form } from 'formik';
+import * as Yup from 'yup';
+import {TextField} from './TextField';
 
 export const Signup = () => {
 
+  const initialValues = {
+    firstName: "",
+    lastName: "",
+    email:"",
+    password:"",
+    confirmPassword:"",
+    aggreeToTerms: false,
+  }
+
   return (
-   <div>
-     <h1>Sign Up</h1>
-   </div>
+   <Formik
+   initialValues={initialValues}
+   
+   >
+     {(formik) => (      
+       <div>
+       <h1 className="my-4 font-weight-bold-display-4">Sign Up</h1>
+       {/* {console.log(formik)} */}
+       <Form>
+         <TextField label="First Name" name="firstName" type="text" />
+         <TextField label="Last Name" name="lastName" type="text" />
+         <TextField label="Email" name="email" type="email" />
+         <TextField label="Password" name="password" type="password" />
+         <TextField label="Confirm Password" name="confirmPassword" type="password" />
+         <TextField label="I accept terms & conditions" name="aggreeToTerms" type="checkbox" />
+         <button className="btn btn-dark mt-3">Register</button>
+         <button className="btn btn-danger mt-3 ml-3">Reset</button>
+       </Form>
+       </div>
+       
+     )}
+   </Formik>
   )
 }
