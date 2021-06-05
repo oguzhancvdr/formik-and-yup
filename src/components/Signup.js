9106d70883512282536909f06cfe44730e5adb13
ogzhn <oguzhancavdar06@gmail.com>
2021-06-05 20:00:50 +0300

    add validation to form

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -13,9 +13,51 @@ export const Signup = () => {
     aggreeToTerms: false,
   }
 
+  const validate = Yup.object({
+    firstName: Yup.string()
+    .min(2, "Too short")
+    .max(15, "Must be 15 char or less")
+    .required("Required"),
+    lastName: Yup.string()
+    .min(2, "Too short")
+    .max(25, "Must be 25 char or less")
+    .required("Required"),
+    email: Yup.string()
+    .email("Email is invalid")
+    .required("Required"),
+    password: Yup.string()
+    .min(8, "Password must be at least 8 char or more")
+    .matches(/\d+/, "Password must have a number")
+    .matches(/[a-z]+/, "Password must have a lowercase")
+    .matches(/[A-Z]+/, "Password must have a uppercase")
+    .matches(/[!?.@#$%^&*()-+]+/, "Password must have a special char")
+    .test("password", "Password is an invalid", value => !/\s+/.test(value))
+    .required("Required"),
+    confirmPassword: Yup.string()
+    .oneOf([Yup.ref("password"), null], "Password must match")
+    .required("Confirming password is required"),
+    aggreeToTerms: Yup.boolean()
+    .label("Terms")
+    .test(
+      "aggreeToTerms", 
+      "Must agree to terms to continue",
+      value => value === true  
+    ),
+  })
+
+  const sleep = (ms) => new Promise((r) => setTimeout(r, ms));
+
   return (
    <Formik
    initialValues={initialValues}
+   validationSchema={validate}
+  //  onSubmit = { values => {
+  //    alert(JSON.stringify(values, null,2))
+  //   }}
+  onSubmit={async (values) => {
+    await sleep(2500);
+    alert(JSON.stringify(values, null,2))
+  }}
    
    >
      {(formik) => (      
@@ -29,8 +71,8 @@ export const Signup = () => {
          <TextField label="Password" name="password" type="password" />
          <TextField label="Confirm Password" name="confirmPassword" type="password" />
          <TextField label="I accept terms & conditions" name="aggreeToTerms" type="checkbox" />
-         <button className="btn btn-dark mt-3">Register</button>
-         <button className="btn btn-danger mt-3 ml-3">Reset</button>
+         <button className="btn btn-dark mt-3" type="submit">Register</button>
+         <button className="btn btn-danger mt-3 ml-3" type="reset">Reset</button>
        </Form>
        </div>
        
--- src/components/TextField.js
+++ src/components/TextField.js
@@ -1,13 +1,16 @@
-import {useField } from 'formik';
+import {useField, ErrorMessage } from 'formik';
 
 export const TextField = ({label, ...props}) => {
   const [field, meta] = useField(props);
-  console.log({field, meta});
- 
+
   return (
     <div className="mb-2">
       <label htmlFor={field.name}>{label}</label>
-      <input  id={field.name}/>
+      <input className={`form-control shadow-none ${ meta.touched &&  meta.error && "is-invalid"} `}
+        {...field} {...props}
+        autoComplete="off"      
+      />
+      <ErrorMessage component="div" name={field.name} className="error" />
     </div>
   )
 }
